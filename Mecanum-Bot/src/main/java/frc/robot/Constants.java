df6451ea78272caa896aca577e2cddeced06805c
caTrixLiu <lyr20070621@gmail.com>
2025-01-08 22:49:57 +0800

    added constant file

---
+++ Mecanum-Bot/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public final class Constants {
+    public static final class DriveConstants {
+        public static final int FRONT_LEFT_MOTOR_ID = 1;
+        public static final int FRONT_RIGHT_MOTOR_ID = 2;
+        public static final int BACK_LEFT_MOTOR_ID = 3;
+        public static final int BACK_RIGHT_MOTOR_ID = 4;
+
+        public static final boolean FRONT_LEFT_MOTOR_INVERTED = false;
+        public static final boolean FRONT_RIGHT_MOTOR_INVERTED = false;
+        public static final boolean BACK_LEFT_MOTOR_INVERTED = false;
+        public static final boolean BACK_RIGHT_MOTOR_INVERTED = false;
+    }
+}
